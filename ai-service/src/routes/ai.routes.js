027a930fa61129e608a7e91f8e5eca31d7fa04e7
Javith06 <miistahjavith@gmail.com>
2026-06-16 12:04:51 +0530

    deleted old codes

--- ai-service/src/routes/ai.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleChat, handleQuery, handleDashboard } = require('../controllers/chat.controller');
-
-router.post('/chat', handleChat);
-router.post('/query', handleQuery);
-router.get('/dashboard', handleDashboard);
-
-module.exports = router;
